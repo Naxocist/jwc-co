a6347897319e1fa4813242d744bc6d8aadd50326
naxocist <subforwork@gmail.com>
2024-07-18 15:20:13 +0700

    Finish basic webpage

--- src/components/Content/Item.jsx
+++ src/components/Content/Item.jsx
@@ -2,18 +2,22 @@ function Item({title, pic, price, discount, stars, percent}) {
 
   const isFloat = Number(stars) === stars  && stars % 1 !== 0 
 
+  const svg_stars = Array.from({ length: stars }).map((_, index) => (
+    <svg key={index} width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.04682 0L11.666 5.63991L17.8393 6.38809L13.2848 10.6219L14.4809 16.7243L9.04682 13.701L3.61278 16.7243L4.80885 10.6219L0.254359 6.38809L6.42761 5.63991L9.04682 0Z" fill="#FFC633" />
+    </svg>
+  ));
+
   return (
     <div>
       <div className="w-[295px] h-[298px] bg-[#F0EEED] rounded-[20px] overflow-hidden relative">
-        <img className="absolute top-[-70px]" src={pic}></img>
+        <img className="absolute top-[-70px] scale-[90%]" src={pic}></img>
       </div>
 
       <p className="font-[700] text-[20px]">{title}</p>
 
       <div className="flex items-center">
-        <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M9.04682 0L11.666 5.63991L17.8393 6.38809L13.2848 10.6219L14.4809 16.7243L9.04682 13.701L3.61278 16.7243L4.80885 10.6219L0.254359 6.38809L6.42761 5.63991L9.04682 0Z" fill="#FFC633" />
-        </svg>
+        {svg_stars}
 
         {
           isFloat && (
@@ -25,7 +29,7 @@ function Item({title, pic, price, discount, stars, percent}) {
 
 
         <div className="ml-[13px]">
-          {stars}{!isFloat && ".0"}/5
+          {stars}{!isFloat && ".0"}/<span className="text-[rgba(0,0,0,.6)]">5</span>
         </div>
 
       </div>
